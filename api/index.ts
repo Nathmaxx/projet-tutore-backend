132ffb3f83bbef81b161e2dbdfe080351c9bfde2
BDC <baptiste.do-cabo@etu.univ-lyon1.fr>
2025-01-23 10:05:42 +0100

    add consommations_industrielles model and API routes for CRUD operations

--- api/index.ts
+++ api/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 import consommations from './routes/consommations/consommations';
 import parcelles from './routes/parcelles/parcelles';
+import consommations_industrielles from './routes/consommations_industrielles/consommations_industrielles';
 
 const router = express.Router();
 
 router.use('/consommations', consommations);
 router.use('/parcelles', parcelles);
+router.use('/consommations_industrielles', consommations_industrielles);
 
 export default router;
---
+++ api/models/consommations_industrielles/consommations_industrielles.ts
@@ -0,0 +1,12 @@
+export type ConsommationsIndustriellesType = {
+    id_consommation_industrielle: string;
+    id_parcelle?: string;
+    i_conso_elec?: number;
+    i_conso_gaz?: number;
+    i_conso_rcu?: number;
+    i_mwh_ef?: number;
+    i_mwh_ep?: number;
+    i_pdl_elec?: number;
+    i_pdl_gaz?: number;
+  };
+  
