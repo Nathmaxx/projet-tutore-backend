70208ae488f55d0db30ab127dcae676db3df4e43
Nathan <nathan.girardi@proton.me>
2025-01-23 10:27:26 +0100

    Ajout consommations tertiaires

--- api/index.ts
+++ api/index.ts
@@ -5,6 +5,7 @@ import parcelles from './routes/parcelles/parcelles';
 import seed from './routes/seed/seed';
 import consommations_industrielles from './routes/consommations_industrielles/consommations_industrielles';
 import consommations_residentielles from './routes/consommations_residentielles/consommations_residentielles';
+import consommations_tertiaires from './routes/consommations_tertiaires/consommations_tertiaires';
 
 const router = express.Router();
 
@@ -13,5 +14,5 @@ router.use('/parcelles', parcelles);
 router.use('/seed', seed);
 router.use('/consommations_industrielles', consommations_industrielles);
 router.use('/consommations_residentielles', consommations_residentielles);
-
+router.use('/consommations_territoriales', consommations_tertiaires );
 export default router;
---
+++ api/models/consommations_tertiaires/consommations_tertiaires.ts
@@ -0,0 +1,13 @@
+export type ConsommationsTertiairesType = {
+    id_consommation: string;
+    id_parcelle?: string;
+    conso_elec?: number;
+    conso_gaz?: number;
+    conso_rcu?: number;
+    mwh_ef?: number;
+    mwh_ep?: number;
+    pdl_elec?: number;
+    pdl_gaz?: number;
+    nb_adresses_livrees?: number;
+  };
+  
